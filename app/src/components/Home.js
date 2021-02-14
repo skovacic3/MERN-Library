d0f3b4cb62f16f3e613e7f7574652578636d9bd7
skovacic3 <sven.kovacic7+github@gmail.com>
2021-02-14 14:23:22 +0100

    edited home page

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Jumbotron from "react-bootstrap/Jumbotron";
 
 const Home = props => {
     return (
-        <div>
-            <p>Home Page</p>
-        </div>
+        <Jumbotron>
+            <h1>Welcome to the MERN library</h1>
+            <p>
+                This page was made in the MERN Stack. Enjoy your stay
+            </p>
+        </Jumbotron>
     );
 };
 
